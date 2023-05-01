69e8980a4b82c3f4b07a7d39d6375fd263edfa42
elian <elian7joyo7villanueva@gmail.com>
2023-05-01 16:52:28 -0500

    search screen initialize

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:ticket_booking_app/utils/app_styles.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: [],
+      ),
+    );
+  }
+}
